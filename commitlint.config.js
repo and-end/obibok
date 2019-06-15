974d7b00b4a1ed6c2415c8cd7ff9ced7f9602d3e
ochmanski <ochmanskikacper@gmail.com>
2019-06-15 21:33:19 +0200

    feat(*): add tslint and typescript

--- commitlint.config.js
+++ commitlint.config.js
@@ -1,14 +1,13 @@
-const angularTypes = require("@commitlint/config-angular-type-enum");
+const angularTypes = require('@commitlint/config-angular-type-enum');
 
 const types = [
-    angularTypes.value(),
-    'chore'
+  angularTypes.value(),
+  'chore'
 ];
 
-
 module.exports = {
-    extends: ['@commitlint/config-angular'],
-    rules: {
-        'type-enum': [2, 'always', types]
-    }
+  extends: ['@commitlint/config-angular'],
+  rules: {
+    'type-enum': [2, 'always', types]
+  }
 };
