72d9759433aab31047d500d27d2b3868d0ed9133
ochmanski <ochmanskikacper@gmail.com>
2019-06-16 15:09:02 +0200

    build(ci): add ability to push back linted and formatted files

---
+++ .travis/push.sh
@@ -0,0 +1,21 @@
+#!/bin/sh
+
+setup_git() {
+  git config --global user.email "travis@travis-ci.org"
+  git config --global user.name "Travis CI"
+}
+
+commit_website_files() {
+  git add .
+  git checkout continuous-integration/travis-ci
+  git commit --message "ci(style): lint and format files"
+}
+
+upload_files() {
+  git remote add origin-obibok https://${GH_TOKEN}@github.com/and-end/obibok.git > /dev/null 2>&1
+  git push --set-upstream origin-obibok
+}
+
+setup_git
+commit_website_files
+upload_files
