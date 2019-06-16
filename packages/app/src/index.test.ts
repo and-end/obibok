05b9c4a2e0f737f0840c752a33efc2f5a3e5c215
Travis CI <obibok-release-bot>
2019-06-16 13:38:23 +0000

    ci(style): lint and format files

--- packages/app/src/index.test.ts
+++ packages/app/src/index.test.ts
@@ -1,8 +1,8 @@
 global.console.log = jest.fn();
 
-describe('app', () => {
-    it('loggs \"Hello app\"', () => {
-        require('./index');
-        expect(global.console.log).toHaveBeenCalledWith('Hello app');
-    });
+describe("app", () => {
+  it('loggs "Hello app"', () => {
+    require("./index");
+    expect(global.console.log).toHaveBeenCalledWith("Hello app");
+  });
 });
