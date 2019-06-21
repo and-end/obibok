7dd7e087438a93d72623ad9bbd0377d6d38cac57
ochmanski <ochmanskikacper@gmail.com>
2019-06-21 21:45:09 +0200

    feat(config): add config manager

---
+++ config/index.js
@@ -0,0 +1,34 @@
+const convict = require('convict');
+const colors = require('colors');
+const util = require('util');
+
+const config = convict({
+  env: {
+    doc: 'The application environment.',
+    format: [
+      'production', "development"],
+    default: 'development',
+    env: 'NODE_ENV'
+  }
+});
+
+const env = config.get('env');
+const configPath = `./config/${env}.json5`;
+
+try {
+  config.loadFile(configPath);
+} catch (error) {
+  console.error(`
+  ${colors.bgRed.bold(` Error `)} in ${colors.bgBlack.red.bold(` ${configPath} `)}
+  
+    ${util.inspect(error, { colors: true })}
+  
+  See ${colors.cyan.bold('https://github.com/and-end/obibok/blob/master/README.md')} for more info.
+  `);
+
+  process.exit(1);
+}
+
+config.validate({ allowed: 'strict' });
+
+module.exports = config;
