9dc8faa1df5bf8ca6dc9eaed23cab23d21482f9c
ochmanski <ochmanskikacper@gmail.com>
2019-08-24 15:21:27 +0200

    refactor(config): remove console log from jest config

--- packages/app/jest.config.js
+++ packages/app/jest.config.js
@@ -3,8 +3,6 @@ const { pathsToModuleNameMapper } = require('ts-jest/utils');
 const makeJestConfig = require('../../utils/makeJestConfig');
 const { compilerOptions } = require('./tsconfig');
 
-console.log(pathsToModuleNameMapper(compilerOptions.paths));
-
 module.exports = makeJestConfig({
   displayName: {
     color: 'yellow'
